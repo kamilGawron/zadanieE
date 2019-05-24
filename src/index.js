436a35766e005112e70ca3d1aaa692908b284fd4
Kamil Gawron <kgawron955@gmail.com>
2019-05-24 21:19:05 +0200

    empty basic react template

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from '../components/App.js'
+require('./index.css');
+
+
+ReactDOM.render(<App />,document.getElementById('root'))
